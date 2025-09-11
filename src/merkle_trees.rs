65c533f5d2f845e9a7438e1f0c6f244488091069
nelson <nelson.antunes994@gmail.com>
2025-09-12 00:09:32 +0200

    chore: progress on merkle tree builder

--- src/merkle_trees.rs
+++ src/merkle_trees.rs
@@ -1,7 +1,19 @@
-use std::path::Path;
+use std::{path::Path, sync::mpsc::channel, thread};
 
-use crate::HashMethod;
+use crossbeam_channel::bounded;
+
+use crate::{HashMethod, md5_hasher::md5_hash_file};
 
 pub fn build_merkle_tree(paths: &Path, method: &HashMethod, bytes_to_hash: u64) {
-    unimplemented!()
+    let mut hash_method;
+    match method {
+        HashMethod::Md5 => {
+            hash_method = md5_hash_file;
+        }
+        _ => unimplemented!(),
+    }
+
+    let workers = thread::available_parallelism().map(|n| n.get()).unwrap_or(4);
+
+    // let (s, r) = bounded(workers);
 }
